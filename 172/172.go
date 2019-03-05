e56eed984f00ae205cf30ba94c6eea03b806afa9
Xie Yuanhang <1021274364@qq.com>
2019-03-05 14:08:12 +0800

    solve 172

---
+++ 172/172.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func trailingZeroes(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return n/5 + trailingZeroes(n/5)
+}
+
+func main() {
+	fmt.Println(trailingZeroes(200))
+}
